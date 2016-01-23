e4871cd904c88b81850c0e16d0b4c6d4bb2d2d83
FLAUCE <flauce_20@hotmail.com>
2016-01-23 09:55:53 -0400

    Update Boss_Announcer.cpp

--- src/server/scripts/Custom/CustomFunStuff/Boss_Announcer.cpp
+++ src/server/scripts/Custom/CustomFunStuff/Boss_Announcer.cpp
@@ -13,7 +13,7 @@ if (boss->isWorldBoss())
 std::string plrname = player->GetName();
 std::string bossname = boss->GetName();	
 char msg[250];
-snprintf(msg, 250, "|CFF7BBEF7[ Boss Announcer ]|r:|cffff0000 %s |r and his group killed world boss |CFF18BE00[%s]|r !!!",plrname.c_str(),bossname.c_str());
+snprintf(msg, 250, "|cffffff00[|r|c00077766Anuncio del Boss|r|cffffff00]:|r|cffff0000 %s |r and his group killed world boss |CFF18BE00[%s]|r !!!",plrname.c_str(),bossname.c_str());
 sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
 }
 }
